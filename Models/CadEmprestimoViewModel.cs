95b34ab6424b62fe86c0666e876359fb76677d35
Mateus Destefani Fabri <mat.dfabri@gmail.com>
2021-08-08 16:05:53 -0300

    Solução problema 7 - lista livros a emprestar

--- Models/CadEmprestimoViewModel.cs
+++ Models/CadEmprestimoViewModel.cs
@@ -6,5 +6,13 @@ namespace Biblioteca.Models
     {
         public ICollection<Livro> Livros { get; set; }
         public Emprestimo Emprestimo { get; set; }
+
+        //Para resolver problema 7
+
+        public ICollection<Livro> ListarDisponiveisCadastroEmprestimo(){
+
+            LivroService livroService = new LivroService();
+            return livroService.ListarDisponiveis();
+        } 
     }
 }
